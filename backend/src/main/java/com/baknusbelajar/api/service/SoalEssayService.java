bc94a50cb092feead81f02e188c443c6aa9824f1
frianprianas <frianprianas@gmail.com>
2026-09-24 21:18:21 +0700

    fix: add @Slf4j annotation to SoalEssayService

--- backend/src/main/java/com/baknusbelajar/api/service/SoalEssayService.java
+++ backend/src/main/java/com/baknusbelajar/api/service/SoalEssayService.java
@@ -7,6 +7,7 @@ import com.baknusbelajar.api.repository.SoalEssayRepository;
 import com.baknusbelajar.api.repository.JawabanSiswaRepository;
 import com.baknusbelajar.api.repository.UjianMapelRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SoalEssayService {
