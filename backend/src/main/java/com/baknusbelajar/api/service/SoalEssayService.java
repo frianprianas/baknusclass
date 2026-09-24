3b7a3e8b539bb485b6fc7907e4c953ee7a473502
frianprianas <frianprianas@gmail.com>
2026-09-24 15:03:37 +0700

    fix: import Transactional in SoalEssayService

--- backend/src/main/java/com/baknusbelajar/api/service/SoalEssayService.java
+++ backend/src/main/java/com/baknusbelajar/api/service/SoalEssayService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
